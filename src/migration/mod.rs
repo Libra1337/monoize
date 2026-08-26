f5abcff1a1be1d1044007be332a1a6dc842c0a9a
Chad Newman <christianwu1020@outlook.com>
2026-08-26 19:27:34 +0800

    chore: register Provider schema contract module

--- src/migration/mod.rs
+++ src/migration/mod.rs
@@ -229,6 +229,7 @@ mod m20260824_000040_drop_request_log_visible_tps;
 mod m20260824_000041_request_capture_records;
 mod m20260825_000042_groups_registry;
 mod m20260825_000043_channel_allow_missing_usage;
+mod m20260826_000044_provider_pricing_flatten;
 
 #[cfg(test)]
 mod tests {
